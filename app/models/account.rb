4e491069565cc89a34a5a6eaeaa25966039b7bfa
amrita pritam <icode.dev7@gmail.com>
2016-01-14 14:34:36 +0530

    commit

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,8 @@
 class Account < ActiveRecord::Base
   validates_presence_of :shopify_account_url
-validates_presence_of :shopify_api_key
+  validates_presence_of :shopify_password
 validates_presence_of :shopify_password
 validates_presence_of :shopify_shared_secret
-
   has_many :orders, :dependent => :destroy
   has_many :products, :dependent => :destroy
   has_many :contests, :dependent => :destroy
