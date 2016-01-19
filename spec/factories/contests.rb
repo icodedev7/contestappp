a531537e05fd3ab33ae0c789604fad2fafbc1705
amrita pritam <icode.dev7@gmail.com>
2016-01-19 12:27:13 +0530

    commit

--- spec/factories/contests.rb
+++ spec/factories/contests.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
   factory :contest do
     name "MyString"
     product_id 1
-    start_date "2014-05-18 23:29:59"
-    end_date "2014-05-18 23:29:59"
+    start_date "2015-01-11 23:29:59"
+    end_date "2016-01-11 23:29:59"
     max_results 1
     order_id 1
     product_name "MyString"
